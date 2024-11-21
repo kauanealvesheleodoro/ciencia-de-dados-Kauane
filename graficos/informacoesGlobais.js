92c60c2cb5a85bc93b3e241b0c256b289f30fb77
kauanealvesheleodoro <107581220+kauanealvesheleodoro@users.noreply.github.com>
2024-11-21 16:42:02 -0300

    21-11

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,1 +1,10 @@
-const url = 'https://raw.githubusercontent.com/kauanealvesheleodoro/ciencia-de-dados-Kauane/refs/heads/main/dados-globais.json';
+const url = 'https://raw.githubusercontent.com/kauanealvesheleodoro/ciencia-de-dados-Kauane/refs/heads/main/dados-globais.json'; 
+
+async function visualizarinformacoesGlobais(){
+    const res = await fetch (url);
+
+    const dados = await res.json();
+}
+    
+
+    
